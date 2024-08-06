bc83597be32554a9da787609c1471fa3a864ec99
Sponton <sosoetiquinho@gmail.com>
2024-08-06 12:22:19 +0000

    [feat] expo-router

--- src/app/index.js
+++ src/app/index.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Minha primeira janela</Text>
+      <Text>Aplicativo pronto paea usar</Text>
       <StatusBar style="auto" />
     </View>
   );
