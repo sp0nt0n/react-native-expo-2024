c01e943dcc76ed414fce0800e7e7ed925cd17fe9
Sponton <sosoetiquinho@gmail.com>
2024-09-10 12:35:28 +0000

    commit prova

--- src/app/_layout.js
+++ src/app/_layout.js
@@ -1,11 +1,39 @@
-import { Slot, Stack } from "expo-router";
+import { Slot, Stack, useSegments, router } from "expo-router";
 import { AppProvider } from "../hooks";
+import { useAuth } from "../hooks/Auth";
+import { useEffect } from "react";
 
+
+const StackLayout = () => {
+    const {user} = useAuth();
+const segments = useSegments();
+
+useEffect(() => {
+    const inAuthGroup = segments[0] === "(protected)";
+
+    if (!user?.autenticated && inAuthGroup){
+        router.replace("/")
+    } else{
+        if (user?.autenticated){
+            router.replace("/(protected)");
+        }
+
+    }
+}, [user]);
+
+    return (
+        <Stack>
+        <Stack.Screen name="index" options={{headerShown: false}}/>
+        <Stack.Screen name="(protected)" options={{headerShown: false}} / >
+    </Stack>
+    );
+};
 export default function Layout(){
+
+
     return (
     <AppProvider>
-    
-        <Stack />
+    <StackLayout />
     </AppProvider>
     );
 }
--- src/app/index.js
+++ src/app/index.js
@@ -1,16 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { BackHandler, Button, StyleSheet, Text, View } from 'react-native';
 import { useAuth } from '../hooks/Auth';
+import { router } from 'expo-router';
 
 export default function App() {
   const {signIn, signOut} = useAuth();
+
+const handleEntrarSuper = async () =>{
+  try{
+    await signIn({email: "super@gmail.com", password: "Super123!"})
+    router.replace("/");
+  } catch(error){
+    console.log(e)
+  }
+  
+}
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Aplicativo pronto para usar</Text>
-      <Button title= "Signin Super" onPress={()=>signIn({email: "superemail@gmail.com", password: "Super123!"})}/>
+      <Button title= "Signin Super" onPress={handleEntrarSuper}/>
       <Button title= "Signin Adm" onPress={()=>signIn({email: "adm@gmail.com", password: "Adm123!"})}/>
       <Button title= "Signin User" onPress={()=>signIn({email: "user@gmail.com", password: "User123!"})}/>
-      <Button title= "Signout" onPress={()=> signOut()}/>
+     <Button title= "Sobre" onPress={() =>router.push("/about")}/>
+      <Button title= "Sair do Aplicativo" onPress={()=> BackHandler.exitApp()}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -22,6 +35,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 15,
   },
   title:{
     fontFamily: "regular",
